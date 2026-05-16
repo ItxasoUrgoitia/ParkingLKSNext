3c4c3d2ba55fe3ad931d21ce0f8f3def6bdae4d3
ItxasoUrgoitia <iurgoitia002@ikasle.ehu.eus>
2026-05-16 22:30:03 +0200

    Fix: Intentando arreglar error en Build and Analyze

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("org.sonarqube") version "4.4.1.3373" apply false
+    id("org.sonarqube") version "7.3.0.8198"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "ItxasoUrgoitia_ParkingLKSNext")
+        property("sonar.organization", "itxasourgoitia")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.androidVariant", "debug")
+    }
 }
